82876e4f63a1a3ffaf8844d3962a1211df02d2d7
Mooventhan <kmooventhan98@gmail.com>
2022-12-21 21:58:49 +0530

    validation initial

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,17 @@
 class BooksController < ApplicationController
   def index
-    render json: Book.all
+    render json: Book.all, status: :ok
+  end
+  def create
+    book = Book.new(book_params)
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unproceaable_entity
+    end
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
   end
 end
